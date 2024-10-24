991bd1952ea7994260880809dd257895eb5eb4fc
litscorpi-star <79298405+litscorpi-star@users.noreply.github.com>
2024-10-24 14:02:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,11 +33,13 @@ def ty_run():
     pwd_ty.extend(ty_pwd.split(","))
     print(user_ty)
     for i in range(len(user_ty)):
+        print(i)
         if user_ty[i] != None and pwd_ty[i] != None:
             content += tianyiyunpan.main(user_ty[i], pwd_ty[i])
             title += "【天翼】"+user_ty[i]
             notify.send(title+' 每日签到',content)
-            time.sleep(120)
+            time.sleep(30)
+            i+=1
         
 if __name__ == "__main__":
     ty_run()
